2bc6848d85582425c0aaa0eee0f311aeeebee1b7
Sahithyan <sahithyan2701@gmail.com>
2020-12-06 16:16:26 +0530

    .\nremoving index.tsx\nmaking sure png files work

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
+import reactLogo from "./assets/logo.png";
 import "./style/main.scss";
 
 export const App = () => {
   return (
     <div>
+      <img className="logo" src={reactLogo} />
       <h1>Hello REACT!</h1>
     </div>
   );
 };
 
-export default App;
+ReactDOM.render(<App />, document.getElementById("app-root"));
